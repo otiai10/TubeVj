3c36900f83bac4525f48fe2b07c156c48940f565
Hiromu OCHIAI <otiai10@gmail.com>
2019-04-17 11:40:57 +0900

    Create separated projects for 2 windows

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 const { app, BrowserWindow } = require('electron');
 
-const __project = 'tube-vj';
 
-const __main__ = () => {
+const __createMasterViewWindow = () => {
+  const __project = 'master-window';
   const win = new BrowserWindow({
     backgroundColor: '#ddd',
     icon: `file://${__dirname}/dist/${__project}/assets/logo.png`,
   });
   win.loadURL(`file://${__dirname}/dist/${__project}/index.html`);
+  return win;
+};
+
+const __createControllerViewWindow = () => {
+  const __project = 'controller-window';
+  const win = new BrowserWindow({
+    backgroundColor: '#ff6347',
+    icon: `file://${__dirname}/dist/${__project}/assets/logo.png`,
+  });
+  win.loadURL(`file://${__dirname}/dist/${__project}/index.html`);
+  return win;
+};
+
+const __main__ = () => {
+  __createMasterViewWindow();
+  __createControllerViewWindow();
 };
 
 app.on('ready', __main__);
